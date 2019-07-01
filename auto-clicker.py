55ed4934da045942379e3b9b5c318974f7289f8f
Jack Dunning <jackdunning23@gmail.com>
2019-07-01 18:03:22 +0100

    basic logic to get user input for start / stop

--- auto-clicker.py
+++ auto-clicker.py
@@ -1,7 +1,34 @@
 import tkinter as tk
+import sys
 
 window = tk.Tk()
 
 window.title("Hello world!")
 
-window.mainloop()
+state = "stop"
+
+def startClicking() :
+    print("Start clicking.")
+    return "start"
+
+def stopClicking() :
+    print("Stop clicking.")
+    return "stop"
+
+# get user input
+command = input("Choose from 'start', 'stop', or 'quit'. =>")
+
+while (command != "quit"):
+    if (command == state):
+        print("Already in that state.")
+    
+    if (command == "start" and state != "start"):
+        state = startClicking()
+    elif (command == "stop" and state != "stop"):
+        state = stopClicking()
+    elif (command != "start" and command != "stop"):
+        print("Unknown command.")
+    # wait for user input
+    command = input("Choose from 'start', 'stop', or 'quit'. => ")
+
+sys.exit()
