5507025b6bcd138da92255dd3d8858c1db9a7dbe
Jacob Rammer <jrammer101@gmail.com>
2019-07-06 19:48:12 -0700

    #3

--- auto_clicker_gui.py
+++ auto_clicker_gui.py
@@ -19,26 +19,31 @@ def handleStartStopPress():
         startStopText.set("START")
         startStopButton.configure(bg="green")
 
+
 def click_time():
     """
-    Set the click time to as low as one second
+    Set the click time to a custom value.
+    Ignores negative values
     """
-    
+
     click_reg = r"(?P<time>[\d]+)"
     get_time = set_time.get()
-    ac.Autoclicker.clicking_time = int(re.search(click_reg, get_time).group())
-
+    time = re.search(click_reg, get_time)
+    if time is not None:  # makde sure a valid time was entered
+        ac.Autoclicker.clicking_time = int(time.group())
 
 
 startStopText = tk.StringVar()
 startStopText.set("START")
-startStopButton = tk.Button(window, textvariable=startStopText, command = handleStartStopPress, bg="green")
+startStopButton = tk.Button(
+    window, textvariable=startStopText, command=handleStartStopPress, bg="green")
 startStopButton.pack(fill=tk.X, padx=5, pady=5, ipadx=20, ipady=20)
 
 set_time = tk.Entry(window, width=5, bg="white")
 set_time.insert(0, "Enter time between clicks: ")
 set_time.pack(side=tk.LEFT, padx=5, pady=5, ipadx=100, ipady=5)
-set_time_button = tk.Button(window, text="Set Time", command=click_time, bg="green")
+set_time_button = tk.Button(window, text="Set Time",
+                            command=click_time, bg="green")
 set_time_button.pack(fill=tk.X, padx=5, pady=5, ipadx=20, ipady=20)
 
 window.mainloop()
