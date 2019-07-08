53ac37800d06ece2a3e45e78950b9338f57b4769
Jack Dunning <jackdunning23@gmail.com>
2019-07-08 22:28:39 +0100

    refactor validation into function + begin a unit test (need to be able to access auto_clicker_gui functions

---
+++ interval_validation_test.py
@@ -0,0 +1,43 @@
+import unittest
+import re
+from auto_clicker_gui import get_validated_interval
+
+class IntervalValidation(unittest.TestCase):
+    def setUp(self):
+        self.click_reg = r"(?P<time>[\d]+)"  # only capture numbers
+
+    def test_basic(self):
+        text_input = get_validated_interval("123")
+        time = re.search(self.click_reg, text_input)
+        self.assertIsNotNone(time)
+        actual_time = int(time.group())
+        self.assertEqual(123, actual_time)
+
+    def test_non_int(self):
+        text_input = "foo"
+        time = re.search(self.click_reg, text_input)
+        self.assertIsNone(time)
+
+    def test_append_int(self):
+        text_input = "foo123"
+        time = re.search(self.click_reg, text_input)
+        self.assertIsNotNone(time)
+        actual_time = int(time.group())
+        self.assertEqual(123, actual_time)
+
+    def test_append_str(self):
+        text_input = "123foo"
+        time = re.search(self.click_reg, text_input)
+        self.assertIsNotNone(time)
+        actual_time = int(time.group())
+        self.assertEqual(123, actual_time)
+
+    def test_negative(self): # currently passing, update to expect -1 returned from validation function
+        text_input = "-123"
+        time = re.search(self.click_reg, text_input)
+        self.assertIsNotNone(time)
+        actual_time = int(time.group())
+        self.assertEqual(123, actual_time)
+
+if __name__ == '__main__':
+    unittest.main()
